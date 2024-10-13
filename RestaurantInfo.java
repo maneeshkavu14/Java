66fc6e7f64df67f55a82d3aa4cb12906a136bea8
maneeshkavu14 <maneeshkavu@gmail.com>
2024-10-13 16:39:57 +0530

    Class and Object Example

---
+++ RestaurantInfo.java
@@ -0,0 +1,200 @@
+public class RestaurantInfo{
+
+	public static void main(String []args){
+
+			Restaurant Restaurant1 = new Restaurant();
+
+			Restaurant1.restoName = "Mehdi";
+			Restaurant1.restoLocation ="Madivala";
+			Restaurant1.restoPhoneNumber= 9090909308l;
+			Restaurant1.restoAverageCost =699; 
+			Restaurant1.restoRating = 10;
+
+
+			System.out.println("Name of Restaurant :"+ Restaurant1.restoName);
+			System.out.println("Location of Restaurant :"+ Restaurant1.restoLocation);
+			System.out.println("ContactNumber of Restaurant :"+ Restaurant1.restoPhoneNumber);
+			System.out.println("Average cost per head in Restaurant :"+ Restaurant1.restoAverageCost);
+			System.out.println("Restaurant rating :"+ Restaurant1.restoRating);
+
+
+			System.out.println("*****************************************************");
+			
+
+
+			Restaurant Restaurant2 = new Restaurant();
+
+			Restaurant2.restoName = "Taj";
+			Restaurant2.restoLocation ="BTM Layout";
+			Restaurant2.restoPhoneNumber= 7090909308l;
+			Restaurant2.restoAverageCost =299; 
+			Restaurant2.restoRating = 8.5f;
+
+
+			System.out.println("Name of Restaurant :"+Restaurant2.restoName);
+			System.out.println("Location of Restaurant :"+Restaurant2.restoLocation);
+			System.out.println("ContactNumber of Restaurant :"+Restaurant2.restoPhoneNumber);
+			System.out.println("Average cost per head in Restaurant :"+Restaurant2.restoAverageCost);
+			System.out.println("Restaurant rating :"+Restaurant2.restoRating);
+
+
+			System.out.println("*****************************************************");
+			
+
+			Restaurant Restaurant3 = new Restaurant();
+
+			Restaurant3.restoName = "Kannur Kitchen";
+			Restaurant3.restoLocation ="Madivala";
+			Restaurant3.restoPhoneNumber= 609090308l;
+			Restaurant3.restoAverageCost =94; 
+			Restaurant3.restoRating = 8.9f;
+
+
+			System.out.println("Name of Restaurant :"+Restaurant3.restoName);
+			System.out.println("Location of Restaurant :"+Restaurant3.restoLocation);
+			System.out.println("ContactNumber of Restaurant :"+Restaurant3.restoPhoneNumber);
+			System.out.println("Average cost per head in Restaurant :"+Restaurant3.restoAverageCost);
+			System.out.println("Restaurant rating :"+Restaurant3.restoRating);
+
+
+			System.out.println("*****************************************************");
+			
+
+			Restaurant Restaurant4 = new Restaurant();
+
+			Restaurant4.restoName = "Manglore Bites";
+			Restaurant4.restoLocation ="Koramangala";
+			Restaurant4.restoPhoneNumber= 709090308l;
+			Restaurant4.restoAverageCost =99; 
+			Restaurant4.restoRating = 7.0f;
+
+
+			System.out.println("Name of Restaurant :"+ Restaurant4.restoName);
+			System.out.println("Location of Restaurant :" + Restaurant4.restoLocation);
+			System.out.println("ContactNumber of Restaurant :"+Restaurant4.restoPhoneNumber);
+			System.out.println("Average cost per head in Restaurant :"+Restaurant4.restoAverageCost);
+			System.out.println("Restaurant rating :"+Restaurant4.restoRating);
+
+
+
+			System.out.println("*****************************************************");
+			
+
+			Restaurant Restaurant5 = new Restaurant();
+
+			Restaurant5.restoName = "Gokul Cafe";
+			Restaurant5.restoLocation ="BTM 2nd Satge";
+			Restaurant5.restoPhoneNumber= 999090308l;
+			Restaurant5.restoAverageCost =25; 
+			Restaurant5.restoRating = 9.5f;
+
+
+			System.out.println("Name of Restaurant :"+Restaurant5.restoName);
+			System.out.println("Location of Restaurant :"+Restaurant5.restoLocation);
+			System.out.println("ContactNumber of Restaurant :"+Restaurant5.restoPhoneNumber);
+			System.out.println("Average cost per head in Restaurant :"+Restaurant5.restoAverageCost);
+			System.out.println("Restaurant rating :"+Restaurant5.restoRating);
+
+
+			System.out.println("*****************************************************");
+			
+
+			Restaurant Restaurant6 = new Restaurant();
+
+			Restaurant6.restoName = "Paragon";
+			Restaurant6.restoLocation ="Bangalore";
+			Restaurant6.restoPhoneNumber= 7678890308l;
+			Restaurant6.restoAverageCost =199; 
+			Restaurant6.restoRating = 9.0f;
+
+
+			System.out.println("Name of Restaurant :"+Restaurant6.restoName);
+			System.out.println("Location of Restaurant :"+Restaurant6.restoLocation);
+			System.out.println("ContactNumber of Restaurant :"+Restaurant6.restoPhoneNumber);
+			System.out.println("Average cost per head in Restaurant :"+Restaurant6.restoAverageCost);
+			System.out.println("Restaurant rating :"+Restaurant6.restoRating);
+
+
+			System.out.println("*****************************************************");
+			
+
+			Restaurant Restaurant7 = new Restaurant();
+
+			Restaurant7.restoName = "Suffy Restaurant";
+			Restaurant7.restoLocation ="Chruch Street";
+			Restaurant7.restoPhoneNumber= 9087690308l;
+			Restaurant7.restoAverageCost =499; 
+			Restaurant7.restoRating = 9.8f;
+
+
+			System.out.println("Name of Restaurant :"+Restaurant7.restoName);
+			System.out.println("Location of Restaurant :"+Restaurant7.restoLocation);
+			System.out.println("ContactNumber of Restaurant :"+Restaurant7.restoPhoneNumber);
+			System.out.println("Average cost per head in Restaurant :"+Restaurant7.restoAverageCost);
+			System.out.println("Restaurant rating :"+Restaurant7.restoRating);
+
+
+
+			System.out.println("*****************************************************");
+			
+
+			Restaurant Restaurant8 = new Restaurant();
+
+			Restaurant8.restoName = "Viceroy Family Restaurant";
+			Restaurant8.restoLocation="HSR Layout";
+			Restaurant8.restoPhoneNumber= 7659902308l;
+			Restaurant8.restoAverageCost =399; 
+			Restaurant8.restoRating = 8.9f;
+
+
+			System.out.println("Name of Restaurant :"+Restaurant8.restoName);
+			System.out.println("Location of Restaurant :"+Restaurant8.restoLocation);
+			System.out.println("ContactNumber of Restaurant :"+Restaurant8.restoPhoneNumber);
+			System.out.println("Average cost per head in Restaurant :"+Restaurant8.restoAverageCost);
+			System.out.println("Restaurant rating :"+Restaurant8.restoRating);
+
+
+
+			System.out.println("*****************************************************");
+			
+
+			Restaurant Restaurant9 = new Restaurant();
+
+			Restaurant9.restoName = "Shankar's Kitchen";
+			Restaurant9.restoLocation="Tirchy";
+			Restaurant9.restoPhoneNumber= 9086903208l;
+			Restaurant9.restoAverageCost =99; 
+			Restaurant9.restoRating =6.0f;
+
+
+			System.out.println("Name of Restaurant :"+Restaurant9.restoName);
+			System.out.println("Location of Restaurant :"+Restaurant9.restoLocation);
+			System.out.println("ContactNumber of Restaurant :"+Restaurant9.restoPhoneNumber);
+			System.out.println("Average cost per head in Restaurant :"+Restaurant9.restoAverageCost);
+			System.out.println("Restaurant rating :"+Restaurant9.restoRating);
+
+
+
+			System.out.println("*****************************************************");
+			
+
+			Restaurant Restaurant10 = new Restaurant();
+
+			Restaurant10.restoName = "Udupi Restaurant";
+			Restaurant10.restoLocation="BTM Layout";
+			Restaurant10.restoPhoneNumber= 7890906308l;
+			Restaurant10.restoAverageCost =120; 
+			Restaurant10.restoRating = 7.5f;
+
+
+			System.out.println("Name of Restaurant :"+Restaurant10.restoName);
+			System.out.println("Location of Restaurant :"+Restaurant10.restoLocation);
+			System.out.println("ContactNumber of Restaurant :"+Restaurant10.restoPhoneNumber);
+			System.out.println("Average cost per head in Restaurant :"+Restaurant10.restoAverageCost);
+			System.out.println("Restaurant rating :"+Restaurant10.restoRating);
+
+
+
+	}
+	
+}
