3dd79507cf3ac51c88a059a864b98c2c974939b0
maneeshkavu14 <maneeshkavu@gmail.com>
2024-10-13 16:39:39 +0530

    Class and Object Example

---
+++ Restaurant.java
@@ -0,0 +1,24 @@
+public class Restaurant{
+	
+	String restoName;
+	String restoLocation;
+	long restoPhoneNumber;
+	double restoAverageCost; 
+	float restoRating;
+
+
+
+	public static void seatCustomer(){
+
+	}
+	public static void serveDish(){
+
+	}
+	public static void increaseCapacity(){
+
+	}
+	public static void displayDetails(){
+		
+	}
+
+}
